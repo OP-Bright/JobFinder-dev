1d6b96712cd8fbf899d8f66e9d7a7055c7f2322f
JaylinJones0 <jonesjaylink@gmail.com>
2025-12-17 10:09:41 -0600

    "Uninstalled uncessary git dependency & got page to render Hello World with express.static"

--- server/index.js
+++ server/index.js
@@ -3,13 +3,10 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.resolve(__dirname, '../dist')));
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log(`App is listening on port http://localhost:${port}`);
