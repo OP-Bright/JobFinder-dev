0c6f27307a21da9e173ef4ab75e7186438e5136f
OP-Bright <rowanbrite@gmail.com>
2025-12-17 19:30:05 -0600

    made basic endpoints, but they need to be connected to the database

--- server/index.js
+++ server/index.js
@@ -33,6 +33,45 @@ app.get('/protected', (req, res) => {
 })
 
 
+// REPORTING:
+
+// Endpoint for READING all reported links.
+app.get('/api/reported-links', (req, res) => {
+  res.sendStatus(501)
+})
+
+// Endpoint for reading a the number of users who have reported a specific link.
+  // Used by the client to see if enough users have reported it.
+  // Also used by the client to display how many users have reported a link
+app.get('/api/reported-links/count', (req, res) => {
+  res.sendStatus(501)
+})
+
+// Endpoint for reading a specific reported link.
+  // this will be used by the client to see if it needs to POST or PATCH the reported link.
+  // this will also be used to see if the current user has already reported this link.
+app.get('/api/reported-links/:link', (req, res) => {
+  res.sendStatus(501)
+})
+
+// Endpoint for CREATING a new reported link.
+app.post('/api/reported-links', (req, res) => {
+  res.sendStatus(501)
+})
+
+// Endpoint for ADDING a user to a reported link. 
+app.patch('/api/reported-links/:link', (req, res) => {
+  res.sendStatus(501)
+})
+
+// Endpoint for DELETING a reported link.
+  // This should basically only be used by developers for now.
+  // This will be really needed if we make a whitelist feature later down the line.
+app.delete('/api/reported-links/:link', (req, res) => {
+  res.sendStatus(501)
+})
+
+
 
 app.listen(port, () => {
   console.log(`App is listening on port http://localhost:${port}`);
